523fd0d114458206ae384fac1569c4f0cdbdd972
Alexandru Scvortov <scvalex@gmail.com>
2009-06-18 20:25:45 +0300

    made configuration modular

--- mkblog.py
+++ mkblog.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 import os
-from django.conf import settings
+import django.conf
 from django.template import Context, Template
 from django.template.loader import render_to_string
 
 TEMPLATE_DIRS = ( "templates", )
-SRC_DIRS = ( "src1", "src2", )
+SRC_DIRS = ( "src", )
 DEST_DIR = "blog"
 
 def makeDestPath(p):
@@ -17,7 +17,16 @@ def makeDestPath(p):
     return p
 
 def main():
-    settings.configure(TEMPLATE_DIRS=TEMPLATE_DIRS)
+    try:
+        import settings
+        global TEMPLATE_DIRS, SRC_DIRS, DEST_DIR
+        TEMPLATE_DIRS = getattr(settings, "TEMPLATE_DIRS", TEMPLATE_DIRS)
+        SRC_DIRS = getattr(settings, "SRC_DIRS", SRC_DIRS)
+        DEST_DIR = getattr(settings, "DEST_DIR", DEST_DIR)
+    except:
+        print "No settings.py found; using default settings."
+    
+    django.conf.settings.configure(TEMPLATE_DIRS=TEMPLATE_DIRS)
 
     for sd in SRC_DIRS:
         for dp, dns, fs in os.walk(sd):
