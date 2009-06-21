6af5f2388197f7885567c51ffb75045f62b90754
Alexandru Scvortov <scvalex@gmail.com>
2009-06-21 20:43:23 +0300

    mkblog.py now works in two phases (read, write)

--- mkblog.py
+++ mkblog.py
@@ -11,13 +11,6 @@ TEMPLATE_DIRS = ( "templates", )
 SRC_DIRS = ( "src", )
 DEST_DIR = "blog"
 
-def makeDestPath(p):
-    """ Ensure that the path exists in the output directory and return it. """
-    p = os.path.join(DEST_DIR, p)
-    try: os.makedirs(p)
-    except: pass # os.makedirs raises error if path exists
-    return p
-
 def getContext(f):
     """ Read associated config file and return read Context. """
     c = {}
@@ -25,12 +18,28 @@ def getContext(f):
         config_dir = os.path.abspath(os.path.dirname(f))
         module_name = os.path.splitext(os.path.basename(f))[0] # name without extension
         sys.path = [config_dir] + sys.path
-        print config_dir, module_name
         c = __import__(module_name).context
         sys.path = sys.path[1:]
     except:
         print "No associated config for %s" % f
     return Context(c)
+
+class BlogEntry:
+    def __init__(self, filepath):
+        if not filepath:
+            raise Exception("No file given.  Comitting suicide.")
+        self.filepath = filepath
+        self.context = getContext(filepath)
+
+    def makeDestPath(self, dd):
+        """ Ensure that the path exists in the output directory and return it. """
+        p = os.path.join(dd, os.path.basename(self.filepath))
+        try: os.makedirs(p)
+        except: pass # os.makedirs raises error if path exists
+        return p
+
+    def write(self, p):
+        open(p, "w").write(Template(open(self.filepath).read()).render(self.context))
 
 def main():
     print "* Reading settings"
@@ -53,19 +62,22 @@ def main():
 
     # Go through dirs, process files, etc.
     print "* Source: %s" % ", ".join(SRC_DIRS)
-    print "* Output: %s" % DEST_DIR
+    es = []
     for sd in SRC_DIRS:
         for dp, dns, fs in os.walk(sd):
             p = dp[len(sd):].lstrip(os.sep) # cut out the top-level dir
             if p.startswith("."):
                 continue # ignore dot files/dirs
-            p = makeDestPath(p) # prepend the dest dir and ensure path exists
-            print "    %s:" % p
             for f in fs:
                 if os.path.splitext(f)[1] in INTERESTING_EXTS:
-                    print "        %s" % f
-                    c = getContext(os.path.join(dp, f))
-                    open(os.path.join(p, f), "w").write(Template(open(os.path.join(dp, f)).read()).render(c))
+                    fp = os.path.join(sd, f)
+                    print "    %s" % fp
+                    es.append(BlogEntry(filepath = fp))
+    print "* Output: %s" % DEST_DIR
+    for e in es:
+        op = e.makeDestPath(DEST_DIR)
+        print "    %s" % op
+        e.write(op)
 
 if __name__ == "__main__":
     main()
